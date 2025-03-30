876604bf0b3520b206a092d7e235af936f1022b0
Andres J Rodriguez <62961520+andresjoantech@users.noreply.github.com>
2025-03-30 17:10:47 -0400

    og

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -17,8 +17,22 @@ const Layout = ({
         <meta name="title" content={title} />
         <meta name="description" content={description} />
 
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://comfy-pithivier-21270c.netlify.app/" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="https://comfy-pithivier-21270c.netlify.app/images/og.jpg" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://comfy-pithivier-21270c.netlify.app/" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content="https://comfy-pithivier-21270c.netlify.app/images/og.jpg" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
           rel="stylesheet"
